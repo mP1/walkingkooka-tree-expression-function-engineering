37c3836fed4d5dcc6cc238337c9adedf85934348
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-04-29 18:26:59 +1000

    EngineeringExpressionFunctions.expressionFunctionProvider ExpressionEvaluationContext type-parameter

--- src/main/java/walkingkooka/tree/expression/function/engineering/EngineeringExpressionFunctions.java
+++ src/main/java/walkingkooka/tree/expression/function/engineering/EngineeringExpressionFunctions.java
@@ -36,7 +36,7 @@ public final class EngineeringExpressionFunctions implements PublicStaticHelper 
     /**
      * An {@link ExpressionFunctionProvider} with all the functions in this project.
      */
-    public static ExpressionFunctionProvider expressionFunctionProvider(final CaseSensitivity nameCaseSensitivity) {
+    public static <C extends ExpressionEvaluationContext> ExpressionFunctionProvider<C> expressionFunctionProvider(final CaseSensitivity nameCaseSensitivity) {
         return ExpressionFunctionProviders
             .basic(
                 Url.parseAbsolute("https://github.com/mP1/walkingkooka-tree-expression-function-engineering/"),
